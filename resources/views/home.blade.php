eaf9c49b92e15366296f40faeb620e768fc556b4
Emmanuel <ademolaemmanuel12@yahoo.com>
2023-06-25 23:16:27 +0100

    Add Navigation and Text widget

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<?php
+/** @var $posts illuminate\Pagination\LengthAwarePaginator */
+?>
+
+<x-app-layout>
+    <!-- Posts Section -->
+    <section class="w-full md:w-2/3 flex flex-col items-center px-3">
+
+        @foreach ($posts as $post)
+            <x-post-item :post="$post"></x-post-item>
+        @endforeach
+
+       
+       
+        <!-- Pagination -->
+        {{ $posts->onEachSide(1)->links() }}
+
+    </section>
+</x-app-layout>
