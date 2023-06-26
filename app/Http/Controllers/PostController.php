8708c4877cf993202c0344967aa614e1989b02c2
Emmanuel <ademolaemmanuel12@yahoo.com>
2023-06-26 01:35:12 +0100

    Implement the view post and its navigation

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -44,7 +44,27 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        //
+        if (!$post->active || $post->published_at == 'NULL') {
+            throw new NotFoundHttpException();
+        }
+
+        $next = Post::query()
+            ->where('active', true)
+            ->whereDate('published_at', '!=', 'NULL')
+            ->whereDate('published_at', '<', $post->published_at)
+            ->orderBy('published_at', 'desc')
+            ->limit(1)
+            ->first();
+
+        $prev = Post::query()
+            ->where('active', true)
+            ->whereDate('published_at', '!=', 'NULL')
+            ->whereDate('published_at', '>', $post->published_at)
+            ->orderBy('published_at', 'asc')
+            ->limit(1)
+            ->first();
+
+        return view('post.view', ['post' => $post, 'prev' => $prev, 'next' => $next]);
     }
 
     /**
